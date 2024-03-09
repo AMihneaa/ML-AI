82132779bf45998c329c40df0035eb20e57441e7
GAlexandru22 <georgescu.alexandru22@gmail.com>
2024-03-09 15:47:06 +0200

    cu citirea fisierelor json

--- cnnModels.py
+++ cnnModels.py
@@ -1,6 +1,6 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
 
 img_width, img_height = 224, 224
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from cerionta import calculate_psnr;
+from cerionta import calculate_psnr
 import json
-from preprocess import preprocess_image;
+from preprocess import preprocess_image
 
 import cv2
 
@@ -8,10 +8,15 @@ if __name__ == "__main__":
     img_original = cv2.imread('../train/000000000139.jpg')
     img_compressed = cv2.imread('../train_noisy/000000000139.jpg')
 
+    f = open('annotations_train.json')
+    train = json.load(f)
+    g = open('annotations_val.json')
+    test = json.load(g)
+
     # Calculați PSNR
     psnr_value = calculate_psnr(img_original, img_compressed)
     print("PSNR:", psnr_value)
 
     img_compressed = preprocess_image(img_compressed)
 
-    print("SDadsad");
+    print("SDadsad")
